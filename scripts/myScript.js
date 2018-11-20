9dd8b76af2df35c46f8752cb04f57d0e29d5237e
meghan <meghan.duprey@gmail.com>
2018-11-20 07:59:23 -0500

    working on javascript slideshow updated main menu with link to blog

--- scripts/myScript.js
+++ scripts/myScript.js
@@ -1,3 +1,14 @@
+//slideshow
+var slides = document.querySelectorAll('#slides .slide');
+var currentSlide = 0;
+var slideInterval = setInterval(nextSlide,2000);
+
+function nextSlide() {
+    slides[currentSlide].className = 'slide';
+    currentSlide = (currentSlide+1)%slides.length;
+    slides[currentSlide].className = 'slide showing';
+}
+
 //function to validate form inputs and alert when something is missing
 function validate() {
     var email = document.forms["contact"]["email"];
