26bb5deacf0ef70dfbceb2b7e39f78b97c8a421b
meghan <meghan.duprey@gmail.com>
2018-11-26 16:13:58 -0500

    added some color to logos and tweaked some other visual things

--- header.php
+++ header.php
@@ -3,6 +3,25 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-129788365-1"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-129788365-1');
+        </script>
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-5B8XPQJ');</script>
+    <!-- End Google Tag Manager -->
+    <meta name="description" content="A personal website about Meghan Duprey">
+    <meta name="robots" content="index,follow">
+    <meta name="google-site-verification" content="EtHFX7GYzlKJdDU6x0er_qGN9NS1Wu99UFGGXu3y0ZE" />
     <title>Meghan Duprey</title>
     <script src="scripts/myScript.js"></script>
     <script src="scripts/jquery-3.3.1.min.js"></script>
@@ -23,6 +42,10 @@
 
   </head>
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5B8XPQJ"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
     <div class="wrapper">
         <header>
             <nav class="navigation" role="navigation">
@@ -30,11 +53,14 @@
                 <ul>
                     <li><a href="index.php">Home</a></li>
                     <li>&middot;</li>
-                    <li><a href="#doing_stuff">I'm Doing Stuff</a></li>
+                    <li><a href="index.php"#doing_stuff>I'm Doing Stuff</a></li>
+                    <li>&middot;</li>
+                    <li><a href="gallery.php">Gallery</a></li>
+                    <li>&middot;</li>
+                    <li><a href="index.php#contact">Contact</a></li>
                     <li>&middot;</li>
                     <li class=""><a href="https://meghanmduprey.wordpress.com/" target="_blank">Blog</a></li>
-                    <li>&middot;</li>
-                    <li><a href="#contact">Contact</a></li>
+                    
                 </ul>
             </nav>
         </header>
